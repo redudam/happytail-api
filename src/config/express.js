4808d19c82371cc75656a78c2fd46a5d86543655
Pavel Fediukovich <pf@ecoinomy.ltd>
2018-11-10 22:46:31 +0300

    Cors helmet disabled

--- src/config/express.js
+++ src/config/express.js
@@ -29,7 +29,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
 // enable CORS - Cross Origin Resource Sharing
-app.use(cors());
+var allowedOrigins = ['http://localhost:3000'];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // enable authentication
 app.use(passport.initialize());
